e3c25daa4e111b665e6a74ff06065b885f83b1e8
Sung Won Chung <sung@datafold.com>
2023-10-24 16:49:06 -0700

    part 1 of dbt mesh

--- models/core/fct_yearly_financials.sql
+++ models/core/fct_yearly_financials.sql
@@ -2,5 +2,5 @@ select
     date_trunc('year', date_month) as date_year
     , sum(cnt_subscribers)         as yearly_subscribers
     , sum(sum_revenue)             as yearly_revenue
-from {{ref('fct_monthly_financials')}}
+from {{ref('fct_monthly_financials', v=1)}}
 group by 1
