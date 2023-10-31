a8a41dc3259e0576ecc1c8a560a607754b989b0e
Sung Won Chung <sung@datafold.com>
2023-10-31 12:56:07 -0700

    add primary keys

--- models/core/fct_yearly_financials.sql
+++ models/core/fct_yearly_financials.sql
@@ -2,5 +2,6 @@ select
     date_trunc('year', date_month) as date_year
     , sum(cnt_subscribers)         as yearly_subscribers
     , sum(sum_revenue)             as yearly_revenue
+    , count(added_column)          as added_column
 from {{ref('fct_monthly_financials')}}
 group by 1
